af857236e0af53a8a4c43ba3da07d2798a9267e6
fran gregori <frangregori61@gmail.com>
2025-06-02 21:17:57 +0200

    add: Open_Closed

---
+++ 02-Open_Closed.ts
@@ -0,0 +1,14 @@
+***
+- Open Closed -
+
+El código debe estar abierto para su extensión pero cerrado para su
+modificación.
+
+Al querer implementar una nueva feature, tenemos que escribir nuevo
+código, no modificar el código existente.
+
+Se agrega código
+No se modifica código
+***
+
+// ejemplo
