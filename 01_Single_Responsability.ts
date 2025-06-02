0291e9b9f2e00d3263e406e0f7dc195d1684c51e
fran gregori <frangregori61@gmail.com>
2025-06-02 20:22:49 +0200

    inital commit add: 01_Single_Responsability

---
+++ 01_Single_Responsability.ts
@@ -0,0 +1,16 @@
+***
+- Single Responsability -
+
+Una clase o un módulo debe tener una única responsabilidad.
+👉 La clase debe cambiar por una sola razón.
+
+1 sola responsabilidad != hacer 1 sola cosa
+1 sola responsabilidad == conjunto de procesos relacionados entre sí
+
+Si tienes más de 1 responsabilidad el código se vuelve:
+- Más rígido
+- Menos flexible
+- Menos tolerante a cambios
+***
+
+// ejemplo
