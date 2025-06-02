8acd970d630c621b731a1a834bfce595d8678f68
fran gregori <frangregori61@gmail.com>
2025-06-02 21:37:50 +0200

    add: 04-Interface_Segregation

---
+++ 04-Interface_Segregation.ts
@@ -0,0 +1,10 @@
+***
+- Interface Segregation -
+
+No obligues a una clase a implementar métodos que no necesita.
+
+Es mejor tener interfaces pequeñas y específicas, que interfaces
+grandes.
+***
+
+// ejemplo
