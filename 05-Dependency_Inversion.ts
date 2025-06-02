25bf15953f1629b777a36cca76f5442c3ce536ad
fran gregori <frangregori61@gmail.com>
2025-06-02 21:51:02 +0200

    add: 05-Dependency_Inversion

---
+++ 05-Dependency_Inversion.ts
@@ -0,0 +1,14 @@
+***
+- Inversión de Dependencias -
+
+Un módulo o un componente necesita de otro para poder realizar su trabajo.
+Pero no debe depender de clases concretas. Sinó de abstracciones. 
+
+Las reglas de negocio o capas de dominio deben depender de abstracciones.
+
+- Tolerante al cambio
+- Facilita el testing
+- Hace visibles tus dependencias
+***
+
+// ejemplo
